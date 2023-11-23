9816127c6ed8fa4b8beb3d19872a7638121d3aae
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2023-11-23 10:38:40 -0300

    feat: ajeitando o projeto

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-
 import lombok.Builder;
 import lombok.Getter;
+
 
 @JsonInclude(Include.NON_NULL)
 @Getter
